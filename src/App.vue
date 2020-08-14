7a2b46c36e9b8384b67c13de1a295fea54aae9d9
JeremieE <jeremie.bauder.jb@gmail.com>
2020-08-14 15:03:57 +0200

    link underligne

--- src/App.vue
+++ src/App.vue
@@ -3,8 +3,8 @@
     <div id="nav" class="block ml-10 pr-4">
       <nav class="flex justify-center items-center mt-4">
           <div class="flex flex-row mt-5">
-            <router-link to="/" class="no-underline mt-3 text-gray-600 mx-3 font-semibold hover:text-gray-500">Joke</router-link>
-            <router-link to="/jokes" class="no-underline mt-3 text-gray-600 mx-3 font-semibold hover:text-gray-500">First jokes</router-link>
+            <router-link to="/" class="mt-3 text-gray-600 mx-3 font-semibold hover:text-gray-500 no-underline">Joke</router-link>
+            <router-link to="/jokes" class="mt-3 text-gray-600 mx-3 font-semibold hover:text-gray-500 no-underline">First jokes</router-link>
           </div>
       </nav>
     </div>
